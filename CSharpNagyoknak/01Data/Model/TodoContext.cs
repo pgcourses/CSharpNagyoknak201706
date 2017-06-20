c38c9ba79185313f19ad4322caec138a5ce98d8c
gplesz <gabor.plesz@gmail.com>
2017-06-20 15:31:08 +0200

    #1: adatbázis paraméterek az App/Web.config-ból

--- CSharpNagyoknak/01Data/Model/TodoContext.cs
+++ CSharpNagyoknak/01Data/Model/TodoContext.cs
@@ -14,6 +14,28 @@ namespace _01Data.Model
 {
     public class TodoContext : DbContext //1. le kell származni a DbContext-ből
     {
+
+        /// <summary>
+        /// A paraméter nélküli létrehozó, ami továbbhívja a paraméterest
+        /// és beállítja paraméterként a DefaultConnection nevű
+        /// kapcsolati karakterláncot.
+        /// </summary>
+        public TodoContext() : this("name=DefaultConnection")
+        { }
+
+        /// <summary>
+        /// Meg akarjuk hívni a bázisosztály paraméteres létrehozóját,
+        /// méghozzá a megfelelő paraméterrel, így készítek
+        /// egy saját paraméteres konstruktort,
+        /// és a bejövő paramétert egyszerűen továbbdobom.
+        /// </summary>
+        /// <param name="nameOrConnectionString">A bázisosztály által elvárt paraméter, 
+        /// vagy egy kapcsolati karakterlánc vagy egy karakterláncnak a neve ami be 
+        /// van állítva pl. az appconfig.ban</param>
+        public TodoContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        { }
+
         //2. Össze kell kötni az adatokat reprezentáló osztályt az elérési osztállyal.
         public DbSet<TodoItem> TodoItems { get; set; }
         public DbSet<Severity> Severities { get; set; }
