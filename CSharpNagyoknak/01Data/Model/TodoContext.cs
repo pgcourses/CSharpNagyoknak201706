c6b8ae3ea5a7e5fd68b45aec54b3432802c584b1
gplesz <gabor.plesz@gmail.com>
2017-06-20 09:55:43 +0200

    #1: Adatelérés beüzemelése  - EntityFramework telepítése  - Adatelérési osztály létrehozása (DbContext)

---
+++ CSharpNagyoknak/01Data/Model/TodoContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Az adatok adatbázisba mentéséhez létre kell hozni az
+/// adatbázissal kapcsolatot tartó osztályt
+/// </summary>
+
+namespace _01Data.Model
+{
+    public class TodoContext : DbContext //1. le kell származni a DbContext-ből
+    {
+        //2. Össze kell kötni az adatokat reprezentáló osztályt az elérési osztállyal.
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ CSharpNagyoknak/01Data/Model/TodoItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Az adatok adatbázisba mentéséhez létre kell hozni az
+/// adatainkat reprezentáló POCO osztályokat
+/// </summary>
+
+namespace _01Data.Model
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime Opened { get; set; }
+        public DateTime Closed { get; set; }
+    }
+}
