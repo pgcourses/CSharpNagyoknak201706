17a14a57b4a7dc4611483715f7e47c0b30106555
gplesz <gabor.plesz@gmail.com>
2017-06-20 10:39:41 +0200

    #1: Első integrációs tesztünk  -Unit teszt projekt  - EntityFramework csomag telepítve  - Data projekt referencia hozzáadva

---
+++ CSharpNagyoknak/01Data.Test/DataTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _01Data.Model;
+using System.Linq;
+
+namespace _01Data.Test
+{
+    /// <summary>
+    /// Figyelem, mivel a TodoContext magával húzza a teljes adatbázist,
+    /// így a tesztjeink nem unit tesztek hanem integrációs tesztek.
+    /// </summary>
+    [TestClass]
+    public class DataTest
+    {
+        [TestMethod]
+        public void DataTest_TodoItems_CountShouldBe0()
+        {
+            //AAA: Act, Arrange, Assert
+
+            //Act
+            var db = new TodoContext();
+
+            //Arrange
+            var count = db.TodoItems.Count();
+
+            //Assert
+            Assert.AreEqual(0, count);
+        }
+    }
+}
