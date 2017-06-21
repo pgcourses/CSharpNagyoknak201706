c6a2ba7df6f5d7f0cb804b0c08a6ab43f9a0fa6d
gplesz <gabor.plesz@gmail.com>
2017-06-21 15:55:20 +0200

    #2, #4: Működő specflow teszt a Genericrepository-ra

---
+++ CSharpNagyoknak/03GenericRepository.Test/GenericRepository.feature
@@ -0,0 +1,23 @@
+﻿Jellemző: GenericRepository
+     Mint fejlesztő, szükségem van egy általános repo osztályra,
+	 ami típusparaméterekkel hívható az egyes adattáblákra
+
+Forgatókönyv: GenericRepo Hozzáadás
+	Adott egy GenericRepo
+	Ha egy új elemet rögzítek
+	Akkor annak látszódnia kell a repoban.
+
+Forgatókönyv: GenericRepo Keresés
+	Adott egy GenericRepo
+	Ha rákeresek egy létező elemre
+	Akkor meg kell találnom
+
+Forgatókönyv: GenericRepo Törlés
+	Adott egy GenericRepo
+	Ha törlök egy létező elemet,
+	Akkor annak el kell tűnnie
+
+Forgatókönyv: GenericRepo Módosítás
+	Adott egy GenericRepo
+	Ha módosítok egy meglévő elemet
+	Akkor a módosításnak látszódnia kell
---
+++ CSharpNagyoknak/03GenericRepository.Test/GenericRepositorySteps.cs
@@ -0,0 +1,83 @@
+﻿using _01Data.Model;
+using _03GenericRepository.AutoMapper;
+using _03GenericRepository.DTO;
+using _03GenericRepository.Repository;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+
+namespace _03GenericRepository.Test
+{
+    [Binding]
+    public class GenericRepositorySteps
+    {
+        private GenericRepository<TodoItem, TodoItemDTO, TodoItemProfile> repo;
+        private TodoItemDTO todoItem;
+
+        [Given(@"egy GenericRepo")]
+        public void AmennyibenEgyGenericRepo()
+        {
+            repo = new GenericRepository<TodoItem, TodoItemDTO, TodoItemProfile>();
+        }
+        
+        [When(@"egy új elemet rögzítek")]
+        public void MajdEgyUjElemetRogzitek()
+        {
+            todoItem = new TodoItemDTO
+            {
+                Title = "Tejet venni a palacsintához",
+                IsDone = false,
+                Opened = DateTime.Now,
+                Closed = null,
+                SeverityId = 3
+            };
+
+            repo.Add(todoItem);
+        }
+        
+        [When(@"rákeresek egy létező elemre")]
+        public void MajdRakeresekEgyLetezoElemre()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"törlök egy létező elemet,")]
+        public void MajdTorlokEgyLetezoElemet()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"módosítok egy meglévő elemet")]
+        public void MajdModositokEgyMeglevoElemet()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"annak látszódnia kell a repoban\.")]
+        public void AkkorAnnakLatszodniaKellARepoban_()
+        {
+            var newTodoItem = repo.Find(todoItem.Id);
+            Console.WriteLine("Új elem visszakeresve: {0}", JsonConvert.SerializeObject(newTodoItem));
+            Assert.IsNotNull(newTodoItem);
+        }
+        
+        [Then(@"meg kell találnom")]
+        public void AkkorMegKellTalalnom()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"annak el kell tűnnie")]
+        public void AkkorAnnakElKellTunnie()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"a módosításnak látszódnia kell")]
+        public void AkkorAModositasnakLatszodniaKell()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
