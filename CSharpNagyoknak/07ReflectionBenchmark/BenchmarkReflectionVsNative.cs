567933074db0c74a4ee000a896e4008a505054c9
gplesz <gabor.plesz@gmail.com>
2017-06-23 15:05:42 +0200

    #7: Első benchmark házilag (ciklus+stopwatch)

---
+++ CSharpNagyoknak/07ReflectionBenchmark/BenchmarkReflectionVsNative.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+
+namespace _07ReflectionBenchmark
+{
+
+    public class BenchmarkReflectionVsNative
+    {
+        private MethodInfo method;
+
+        public BenchmarkReflectionVsNative()
+        {
+            var t = Assembly.GetExecutingAssembly()
+                            .GetType("_07ReflectionBenchmark.BenchmarkReflectionVsNative");
+            method = t.GetMethod("GetDate", BindingFlags.Static | BindingFlags.NonPublic);
+        }
+
+        public object GetDateWithReflection()
+        {
+            return method.Invoke(null, null);
+        }
+
+        public string GetDateWithReflectionFull()
+        {
+            var t = Assembly.GetExecutingAssembly()
+                            .GetType("_07ReflectionBenchmark.BenchmarkReflectionVsNative");
+            var m = t.GetMethod("GetDate", BindingFlags.Static | BindingFlags.NonPublic);
+            return (string)m.Invoke(null, null);
+        }
+
+        public string GetDateWithoutReflection()
+        {
+            return GetDate();
+        }
+
+        static string GetDate()
+        {
+            return DateTime.Now.ToString();
+        }
+
+    }
+}
---
+++ CSharpNagyoknak/07ReflectionBenchmark/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07ReflectionBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+
+            var test = new BenchmarkReflectionVsNative();
+
+            var loop = 1000000;
+
+            sw.Restart();
+            for (int i = 0; i < loop; i++)
+            {
+                var result = test.GetDateWithoutReflection();
+            }
+            Console.WriteLine("Without Reflection: {0}", sw.ElapsedMilliseconds);
+
+            sw.Restart();
+            for (int i = 0; i < loop; i++)
+            {
+                var result = test.GetDateWithReflection();
+            }
+            Console.WriteLine("With Reflection: {0}", sw.ElapsedMilliseconds);
+
+            sw.Restart();
+            for (int i = 0; i < loop; i++)
+            {
+                var result = test.GetDateWithReflectionFull();
+            }
+            Console.WriteLine("With Reflection and preparing: {0}", sw.ElapsedMilliseconds);
+
+            //Without Reflection: 1502
+            //With Reflection: 1652
+            //With Reflection and preparing: 3438
+
+        }
+    }
+}
