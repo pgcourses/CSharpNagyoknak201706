cee5ea2d3c3924178a3165c9db83999c2c2b2647
gplesz <gabor.plesz@gmail.com>
2017-06-23 15:41:37 +0200

    #7: GC benchmark előkészítése

---
+++ CSharpNagyoknak/08GarbageCollectorPerformance/TesztOsztaly.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08GarbageCollectorPerformance
+{
+    internal class TesztOsztaly : IDisposable
+    {
+        List<string> adat = new List<string>();
+
+        public TesztOsztaly()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                adat.Add(new string(' ', 1000));
+            }
+        }
+
+        ~TesztOsztaly()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool isDispose)
+        {
+            if (isDispose)
+            {
+                adat.Clear();
+            }
+        }
+    }
+}
